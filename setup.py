b1dab90a3807a8a9f5a344b15c3bf735050a242c
Glyph <glyph@twistedmatrix.com>
2021-11-17 01:10:14 -0800

    bump version for readme fix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cloud2zone",
-    version="0.1.1",
+    version="0.1.2",
     long_description=open("README.rst").read(),
     packages=find_packages("src"),
     package_dir={"": "src"},
